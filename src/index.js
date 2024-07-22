ce73a37eb825e6aa961a37482a505a79e278a219
Sise-Tounkara <148393450+Sise-Tounkara@users.noreply.github.com>
2024-07-22 23:02:50 +0100

    Add search functionality using an API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input"); 
+function displayData(response) {
+  let currentTemperature = document.querySelector("#weather-app-temperature");
+  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  cityElement.innerHTML = response.data.city;
 }
 
+function searchCity(city) {
+  let apiKey = "1eo9e49738c1c8cbf989d040tcbb916a";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayData);
+}
 
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cardiff");
