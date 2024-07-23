b011681c794c338b7c2c630d7d576d30f4545c2b
Sise-Tounkara <148393450+Sise-Tounkara@users.noreply.github.com>
2024-07-24 00:30:14 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
+function formatDate(date) {
+  let day = date.getDay();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${days[day]} ${hours}:${minutes}`;
+}
+
+
 function displayData(response) {
   let currentTemperature = document.querySelector("#weather-app-temperature");
-  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#weather-app-city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = response.data.temperature.humidity;
+  windSpeed.innerHTML = response.data.wind.speed;
+  timeElement.innerHTML = formatDate(date);
 }
+
+
 
 function searchCity(city) {
   let apiKey = "1eo9e49738c1c8cbf989d040tcbb916a";
