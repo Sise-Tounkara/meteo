f2deedd1099069301612f8b76d298c912f778268
Sise-Tounkara <148393450+Sise-Tounkara@users.noreply.github.com>
2024-07-24 01:00:06 +0100

    Added Icon element

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ function formatDate(date) {
   let day = date.getDay();
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   if (hours < 10) {
     hours = `0${hours}`;
@@ -13,7 +21,6 @@ function formatDate(date) {
   return `${days[day]} ${hours}:${minutes}`;
 }
 
-
 function displayData(response) {
   let currentTemperature = document.querySelector("#weather-app-temperature");
   let cityElement = document.querySelector("#weather-app-city");
@@ -22,16 +29,22 @@ function displayData(response) {
   let windSpeed = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
+
+
 
   currentTemperature.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   description.innerHTML = response.data.condition.description;
   humidity.innerHTML = response.data.temperature.humidity;
   windSpeed.innerHTML = response.data.wind.speed;
-  timeElement.innerHTML = formatDate(date);
+  timeElement.innerHTML = formatDate(date); 
+  icon.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                alt=""
+                class="weather-app-icon"
+              />`;
 }
-
-
 
 function searchCity(city) {
   let apiKey = "1eo9e49738c1c8cbf989d040tcbb916a";
